33d841dd8be5a1daa2455356dc7b511928a67d9d
jaikrishna <84390083+jaikrishnavr@users.noreply.github.com>
2023-01-04 21:56:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <h1>welcome to my random image website </h1>
+      
+      <img src="https://source.unsplash.com/random/200x200?sig=1" />
+
+    </div>
+  );
+}
+
+export default App;
